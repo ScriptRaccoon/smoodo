f49a4e75066d36cb6d3ffe50b7ca8502916e5026
Script Raccoon <scriptraccoon@gmail.com>
2025-05-07 01:49:31 +0200

    radio instead of select

--- src/routes/app/[date]/+page.svelte
+++ src/routes/app/[date]/+page.svelte
@@ -16,22 +16,25 @@
 
 <form method="POST" use:enhance>
 	<input type="hidden" name="date" value={data.date} />
-	<div>
-		<label for="mood">Mood (1 to 5)</label>
-		<select name="mood" id="mood" required>
-			{#each { length: 5 } as _, i}
-				<option value={i + 1} selected={data.mood?.value === i + 1}>
-					{i + 1}
-				</option>
-			{/each}
-		</select>
-	</div>
+	{#each { length: 5 } as _, i}
+		<label>
+			<input
+				type="radio"
+				name="mood"
+				value={i + 1}
+				checked={data.mood?.value === i + 1}
+				required
+			/>
+			{i + 1} &nbsp;
+		</label>
+	{/each}
 
 	<div>
 		<label for="comment">Comment (optional)</label>
-		<textarea name="comment" id="comment" rows="4">
-			{data.mood?.comment ?? ''}
-		</textarea>
+		<br />
+		<textarea name="comment" id="comment" rows="4"
+			>{data.mood?.comment ?? ''}</textarea
+		>
 	</div>
 
 	<button type="submit">
@@ -50,9 +53,3 @@
 {#if form?.success}
 	<p>Mood has been submitted</p>
 {/if}
-
-<style>
-	label {
-		display: block;
-	}
-</style>
