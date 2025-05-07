454eb4edaa869f0206d451dadbf065bf81e2bb98
Script Raccoon <scriptraccoon@gmail.com>
2025-05-07 16:31:14 +0200

    explain features

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,51 @@
 <header class="header">
 	<h1>Smoody</h1>
 </header>
+
+<section>
+	<h2>Track Your Daily Mood</h2>
+	<p>
+		Save a mood for every day and keep a record of how you feel over time. Optionally,
+		add a comment to reflect on your day in more detail.
+	</p>
+</section>
+
+<section>
+	<h2>Visualize Your Moods</h2>
+	<p>
+		Get an overview of all your moods in a color-coded calendar. Easily spot patterns
+		and trends in your emotional well-being.
+	</p>
+</section>
+
+<section>
+	<h2>Secure and Accessible</h2>
+	<p>
+		Enjoy secure user authentication and access your data across all your devices.
+		Smoody is mobile-friendly, so you can use it wherever you are.
+	</p>
+</section>
+
+<section>
+	<h2>Take Control of Your Data</h2>
+	<p>
+		Export all your data at any time to keep a personal backup or analyze it further.
+	</p>
+</section>
+
+<section>
+	<h2>Open Source</h2>
+	<p>
+		Smoody is open source and available on <a
+			href="http://github.com/ScriptRaccoon/smoody"
+			target="_blank"
+			rel="noopener noreferrer">GitHub</a
+		>. Contributions are welcome!
+	</p>
+</section>
+
+<style>
+	h2 {
+		margin-block: 1.5rem -0.5rem;
+	}
+</style>
