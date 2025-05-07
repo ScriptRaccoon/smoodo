7b68a490413b659524bdab7e765d11a2ee7034cb
Script Raccoon <scriptraccoon@gmail.com>
2025-05-07 16:39:08 +0200

    add title and desc

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,15 @@
 	let { children } = $props()
 </script>
 
+<svelte:head>
+	<title>Smoodo - Mood Tracker</title>
+	<meta name="title" content="Smoodo - Mood Tracker" />
+	<meta
+		name="description"
+		content="Smoodo is a mood tracker that helps you keep track of your daily moods and visualize them over time."
+	/>
+</svelte:head>
+
 <Nav />
 
 <main>
