7c0355e6346a1ab19535cc650b6e21abcdba10f5
Script Raccoon <scriptraccoon@gmail.com>
2025-05-07 23:44:42 +0200

    update samples

--- database/sample.js
+++ database/sample.js
@@ -5,6 +5,27 @@ import { writeFileSync } from 'fs'
 const start = new Date('2024-07-04')
 const end = new Date()
 
+const weights = {
+	1: 1,
+	2: 1,
+	3: 4,
+	4: 2,
+	5: 2
+}
+
+function get_random_value() {
+	const entries = Object.entries(weights)
+	const total = entries.reduce((sum, [_, weight]) => sum + weight, 0)
+	let random = Math.random() * total
+
+	for (const [value, weight] of entries) {
+		if (random < weight) {
+			return parseInt(value)
+		}
+		random -= weight
+	}
+}
+
 /**
  * Generates a SQL file with sample data for the database.
  * Requires that a user with id = 1 has been created before.
@@ -15,7 +36,7 @@ function generate_sample_data() {
 	const moods = []
 
 	for (const d = new Date(start); d <= end; d.setDate(d.getDate() + 1)) {
-		const value = Math.floor(Math.random() * 5) + 1
+		const value = get_random_value()
 		const date = d.toISOString().split('T')[0]
 		moods.push({ value, date })
 	}
